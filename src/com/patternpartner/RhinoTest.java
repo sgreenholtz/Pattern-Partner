5c5a03ccf43ff5b43ad87174db7e447105c2a4fc
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-03-03 12:36:21 -0600

    Added Rhino package for Javascript parsing

---
+++ src/com/patternpartner/RhinoTest.java
@@ -0,0 +1,29 @@
+package com.patternpartner;
+
+import org.mozilla.javascript.*;
+
+/**
+ * @author Sebastian Greenholtz
+ */
+public class RhinoTest {
+    public static void main(String[] args) {
+        Context cx = Context.enter();
+        try {
+            Scriptable scope = cx.initStandardObjects();
+
+            String s = "x = 7;";
+
+            cx.evaluateString(scope, s, "<cmd>", 1, null);
+
+            Object x = scope.get("x", scope);
+            if (x == Scriptable.NOT_FOUND) {
+                System.out.println("x is not defined.");g
+            } else {
+                Integer intValue = Integer.parseInt(Context.toString(x));
+            }
+
+        } finally {
+            Context.exit();
+        }
+    }
+}
