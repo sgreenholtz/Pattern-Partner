9f1b314d7c6087df16820596621ef5ad1132c33c
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-03-06 08:40:37 -0600

    Compiled

--- src/com/patternpartner/LoadProperties.java
+++ src/com/patternpartner/LoadProperties.java
@@ -9,11 +9,24 @@ import java.util.*;
  */
 public class LoadProperties {
 
+    private Properties properties;
+
+    /**
+     * Test output of the Properties file with a simple toString
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        LoadProperties propertyLoad = new LoadProperties();
+        System.out.println(propertyLoad.loadProperties("patternpartner.properties"));
+    }
+
     /**
      * Loads properties from properties file
+     * @param propertiesPath String path to the properties file
+     * @return Properties object with the loaded file
      */
     public Properties loadProperties(String propertiesPath) {
-        Properties properties = new Properties();
+        properties = new Properties();
         try {
             properties.load(this.getClass().getResourceAsStream(propertiesPath));
         }
@@ -27,5 +40,15 @@ public class LoadProperties {
         }
 
         return properties;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        Set<String> propertyNames = properties.stringPropertyNames();
+        for (String name : propertyNames) {
+            output += name + System.lineSeparator();
+        }
+        return output;
     }
 }
