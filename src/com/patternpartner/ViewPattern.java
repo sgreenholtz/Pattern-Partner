2217994fa2130fbfe5dae5908b5e59c827fb8cb7
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-03-06 09:16:09 -0600

    Added ViewPattern code for viewing if a pattern is knit or crochet

--- src/com/patternpartner/ViewPattern.java
+++ src/com/patternpartner/ViewPattern.java
@@ -28,7 +28,7 @@ public class ViewPattern {
 
     /**
      * Selects all pattern titles from the database for a specified user
-     * @return an ArrayList where the patternID is mapped to the title
+     * @return Map where the patternID is mapped to the title
      */
     public Map<Integer, String> getAllPatternTitles() {
         Map<Integer, String> titles = new HashMap<>();
@@ -45,6 +45,33 @@ public class ViewPattern {
 
             while (patternsResult.next()) {
                 titles.put(patternsResult.getInt("patternID"), patternsResult.getString("title"));
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+        return titles;
+    }
+
+    /**
+     * Selects all patterns from database for specified user and returns a Map with the patternID of the pattern
+     * and the letter "k" or "c" designating if they are knit or crochet patterns
+     * @return map with patternID mapped to whether the pattern is a knit pattern or crochet pattern
+     */
+    public Map<Integer, String> getKnitOrCrochet() {
+        Map<Integer, String> titles = new HashMap<>();
+        try {
+            Connection conn = DriverManager.getConnection(properties.getProperty("db.url"),
+                    properties.getProperty("db.user"), properties.getProperty("db.password"));
+
+            Statement selectStatement = conn.createStatement();
+
+            String selectSQL = "select patternID, knitOrCrochet from Patterns" +
+                    "where username='" + username + "'";
+
+            ResultSet patternsResult = selectStatement.executeQuery(selectSQL);
+
+            while (patternsResult.next()) {
+                titles.put(patternsResult.getInt("patternID"), patternsResult.getString("knitOrCrochet"));
             }
         } catch (SQLException sqlEx) {
             sqlEx.printStackTrace();
--- web/patternLibrary.jsp
+++ web/patternLibrary.jsp
@@ -6,6 +6,14 @@
   Time: 2:40 PM
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<% if (!(session.getAttribute("username") == null || session.isNew())) { %>
+<%@ page import="com.patternpartner.ViewPattern" %>
 <jsp:include page="header.jsp"/>
+<% ViewPattern viewer = new ViewPattern((String) session.getAttribute("username"));
 
+
+%>
 <jsp:include page="footer.jsp"/>
+<% } else {
+    response.sendRedirect("logIn.jsp");
+} %>
