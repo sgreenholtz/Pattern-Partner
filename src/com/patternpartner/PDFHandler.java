34f931913d26db1a7281592ee2afc483633e8d02
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-03-20 16:51:18 -0500

    Working on PDF parsing

--- src/com/patternpartner/PDFHandler.java
+++ src/com/patternpartner/PDFHandler.java
@@ -16,16 +16,33 @@ public class PDFHandler {
     /**
      * Takes in a PDF file and extracts text from it in String form
      * @param file PDF file to extract text from
-     * @throws IOException if there is an error reading the file
      */
-    public static void getPDFTextFromFile (File file) throws IOException {
-        PDDocument pdfdoc = PDDocument.load(file);
-        String docText = new PDFTextStripper().getText(pdfdoc);
-        BufferedReader reader = new BufferedReader(new StringReader(docText));
-        ArrayList<String> lines = new ArrayList<String>();
-        while (reader.ready()) {
-            String line = reader.readLine();
+    public static void getPDFTextFromFile (File file) {
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("temp_file.txt")));
+        BufferedReader reader = new BufferedReader(new FileReader("temp_file.txt"))) {
+            PDDocument pdfdoc = PDDocument.load(file);
+            String docText = new PDFTextStripper().getText(pdfdoc);
+
+//        BufferedReader reader = new BufferedReader(new StringReader(docText));
+//        ArrayList<String> lines = new ArrayList<String>();
+//        while (reader.ready()) {
+//            String line = reader.readLine();
+//        }
+            writer.print(docText);
+            pdfdoc.close();
+
+            int counter = 0;
+            while (reader.ready()) {
+                String line = reader.readLine();
+                if (line.matches("\\S")) {
+                    System.out.println(counter);
+                } else {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        pdfdoc.close();
     }
 }
