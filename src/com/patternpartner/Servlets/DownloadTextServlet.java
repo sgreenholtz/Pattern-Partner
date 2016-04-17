37519db78e6a2d7f0738731c5f208b1fa9829c06
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-04-17 08:31:43 -0500

    The download is working but not writing the text

--- src/com/patternpartner/Servlets/DownloadTextServlet.java
+++ src/com/patternpartner/Servlets/DownloadTextServlet.java
@@ -3,14 +3,9 @@ package com.patternpartner.Servlets;
 import com.patternpartner.Pattern;
 import com.patternpartner.ViewPattern;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
 
 /**
  * Allows users to download the pattern as a TXT file for editing and re-uploading
@@ -30,10 +25,9 @@ public class DownloadTextServlet extends HttpServlet {
         Integer patternID = new Integer(request.getParameter("id"));
         ViewPattern patternViewer = new ViewPattern();
         Pattern pattern = patternViewer.getPattern(patternID);
-
-        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(
-                        System.getProperty("java.io.tmpdir") + "/"
-                        + pattern.getName().replace(" ", "") + ".txt")))) {
+        String fileLocation = System.getProperty("java.io.tmpdir") + "/";
+        String fileName = pattern.getName().replace(" ", "") + ".txt";
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileLocation + fileName)))) {
             writer.println(pattern.getName());
             writer.println(pattern.listToString(pattern.getDescription()));
             writer.println("Materials");
@@ -45,8 +39,34 @@ public class DownloadTextServlet extends HttpServlet {
                 writer.println(row);
             }
 
-            String url = "patternLibrary";
-            response.sendRedirect(url);
+//            String url = "patternLibrary";
+//            response.sendRedirect(url);
+
+
+            // Find this file id in database to get file name, and file type
+
+            // You must tell the browser the file type you are going to send
+            // for example application/pdf, text/plain, text/html, image/jpg
+            response.setContentType("text/plain");
+
+            // Make sure to show the download dialog
+            response.setHeader("Content-disposition","attachment; filename=" + fileName);
+
+            // Assume file name is retrieved from database
+            // For example D:\\file\\test.pdf
+
+            File my_file = new File(fileLocation + fileName);
+
+            // This should send the file to browser
+            OutputStream out = response.getOutputStream();
+            FileInputStream in = new FileInputStream(my_file);
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = in.read(buffer)) > 0){
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.flush();
 
         } catch (IOException ex) {
             ex.printStackTrace();
