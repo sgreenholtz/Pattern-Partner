0f81d1a824afa52d30d20ff49c444ebda87e7010
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-12-31 18:30:01 -0600

    Adding enums for standards

---
+++ src/main/java/com/patternpartner/entities/CrochetHooks.java
@@ -0,0 +1,9 @@
+package com.patternpartner.entities;
+
+/**
+ * Enum of common crochet hook sizes.
+ * <p>Based on <a href="http://www.craftyarncouncil.com/hooks.html">Craft Yarn Council - Hook Sizes</a></p>
+ * @author Sebastian Greenholtz
+ */
+public enum CrochetHooks {
+}
---
+++ src/main/java/com/patternpartner/entities/KnitNeedles.java
@@ -0,0 +1,9 @@
+package com.patternpartner.entities;
+
+/**
+ * Enum for common knitting needle sizes.
+ * <p>Based on <a href="http://www.craftyarncouncil.com/hooks.html">Craft Yarn Council - Hook Sizes</a></p>
+ * @author Sebastian Greenholtz
+ */
+public enum KnitNeedles {
+}
