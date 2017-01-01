83a5d2bca2f23a4fc9a2b6ca8dbe1a52e989e416
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-12-31 18:47:38 -0600

    Adding enums for crochet hooks and knitting needles

--- src/main/java/com/patternpartner/entities/CrochetHooks.java
+++ src/main/java/com/patternpartner/entities/CrochetHooks.java
@@ -1,9 +1,36 @@
 package com.patternpartner.entities;
 
 /**
- * Enum of common crochet hook sizes.
+ * Enum of common crochet hook sizes. The Enum values are the US letter values,
+ * with the millimeter value available as a double.
  * <p>Based on <a href="http://www.craftyarncouncil.com/hooks.html">Craft Yarn Council - Hook Sizes</a></p>
  * @author Sebastian Greenholtz
  */
 public enum CrochetHooks {
+    B(2.25),
+    C(2.75),
+    D(3.25),
+    E(3.5),
+    F(3.75),
+    G(4),
+    H(5),
+    I(5.5),
+    J(6),
+    K(6.5),
+    L(8),
+    MN(9),
+    NP(10),
+    PQ(15),
+    Q(16),
+    S(19);
+
+    private final double millimeterMeasure;
+
+    CrochetHooks(double mm) {
+        this.millimeterMeasure = mm;
+    }
+
+    double getMillimeterMeasure() {
+        return millimeterMeasure;
+    }
 }
--- src/main/java/com/patternpartner/entities/KnitNeedles.java
+++ src/main/java/com/patternpartner/entities/KnitNeedles.java
@@ -1,9 +1,38 @@
 package com.patternpartner.entities;
 
 /**
- * Enum for common knitting needle sizes.
+ * Enum for common knitting needle sizes. The Enum values are the US names spelled out,
+ * with the millimeter value available as a double.
  * <p>Based on <a href="http://www.craftyarncouncil.com/hooks.html">Craft Yarn Council - Hook Sizes</a></p>
  * @author Sebastian Greenholtz
  */
 public enum KnitNeedles {
+    ONE(2.25),
+    TWO(2.75),
+    THREE(3.25),
+    FOUR(3.5),
+    FIVE(3.75),
+    SIX(4),
+    SEVEN(4.5),
+    EIGHT(5),
+    NINE(5.5),
+    TEN(6),
+    TENHALF(6.5),
+    ELEVEN(8),
+    THIRTEEN(9),
+    FIFTEEN(10),
+    SEVENTEEN(12.75),
+    NINETEEN(15),
+    THIRTYFIVE(19);
+
+    private final double millimeterMeasure;
+
+    KnitNeedles(double mm) {
+        this.millimeterMeasure = mm;
+    }
+
+    double getMillimeterMeasure() {
+        return millimeterMeasure;
+    }
+
 }
