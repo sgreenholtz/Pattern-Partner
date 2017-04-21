64e559a34c0bb54f45c164be73d5c73e5e6f2a71
Sebastian Greenholtz <smgreenholtz@gmail.com>
2017-04-20 22:09:07 -0500

    completed setup

--- react/patternpartner/webpack.config.js
+++ react/patternpartner/webpack.config.js
@@ -1,10 +1,24 @@
+var HTMLWebPackPlugin = require('html-webpack-plugin');
+var HTMLWebPackPluginConfig = new HTMLWebPackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: __dirname + '/app/index.js',
   module: {
     loaders: [
       {
-        test: /\.js$/
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
-  }
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebPackPluginConfig]
 };
