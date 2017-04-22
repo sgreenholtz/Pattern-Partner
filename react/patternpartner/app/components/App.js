0f7f49eda9f711b58d7310526fdeb8caccc08f1d
Sebastian Greenholtz <smgreenholtz@gmail.com>
2017-04-21 19:23:53 -0500

    Playing with styles

--- react/patternpartner/app/components/App.js
+++ react/patternpartner/app/components/App.js
@@ -1,9 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var Nav = require('./nav');
 var App = React.createClass({
   render: function() {
-    return <h1>Hello World!</h1>;
+    return <Nav />;
   }
 });
 
